62ccf00ed6550f0e30cd0178c37b0f18618897aa
Austin Holzer <amholzer12@gmail.com>
2022-10-23 12:44:52 -0700

    switch RPS game to buttons as user input

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-
+const result = document.querySelector('#result');
+const playerScoreDisplay = document.querySelector('#playerScoreDisplay');
+const computerScoreDisplay = document.querySelector('#computerScoreDisplay');
+let playerScore = 0;
+let computerScore = 0;
 
 /* generate a random number between 0 and 2 and return a unique result of rock, paper, or
-    scissors */
+scissors */
 function getComputerChoice() {
     randomNum = Math.floor(Math.random() * 3)
     if (randomNum == 0) {
@@ -19,59 +23,85 @@ function playRound(computerSelection, playerSelection) {
     // console.log(playerSelection)
     if (computerSelection == "rock") {
         if (playerSelectionLowerCase == "rock") {
-            return "Tie"
+            return "Tie";
         } else if (playerSelectionLowerCase == "paper") {
-            return "Win"
+            playerScore++;
+            return "Win";
         } else {
-            return "Lose"
+            computerScore++;
+            return "Lose";
         }
     } else if (computerSelection == "paper") {
         if (playerSelectionLowerCase == "rock") {
-            return "Lose"
+            computerScore++;
+            return "Lose";
         } else if (playerSelectionLowerCase == "paper") {
-            return "Tie"
+            return "Tie";
         } else {
-            return "Win"
+            playerScore++;
+            return "Win";
         }
     } else {
         if (playerSelectionLowerCase == "rock") {
-            return "Win"
+            playerScore++;
+            return "Win";
         } else if (playerSelectionLowerCase == "paper") {
-            return "Lose"
+            computerScore++;
+            return "Lose";
         } else {
-            return "Tie"
+            return "Tie";
         }
     }
-
-    // TO DO!!! determines who wins and why with case sensitity
+    
 }
-function game() {
-    let runningScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let choice = prompt("Choose your weapon", "rock, paper, or scissors!")
-        const result = playRound(getComputerChoice(), choice)
-        console.log(result);
-        // console.log(`You ${result} the round`);
-        if(result == "Win") {
-            // console.log("You win the round");
-            //we do something
-            runningScore++;
-        } else if(result == "Lose") {
-            // console.log("You lose the round");
-            runningScore--;
-        } else {
-            runningScore + 0;
-        }
-    }
-    if (runningScore > 0) {
-        return "You win the game!"
-    } else if (runningScore < 0) {
-        return "You lose the game."
-    } else {
-        return "You tied, play again!"
+function scoreDisplay(answer) {
+    result.textContent = `Results ${answer}`
+    playerScoreDisplay.textContent = `Player Score ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer Score ${computerScore}`;
+    const finalResult = getResult();
+    if (finalResult) {
+        //use setTimeout to fix score numbers updating before final result is displayed
+        setTimeout(function(){
+            alert(finalResult);
+        }, 0)
+    
     }
 }
+    
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', (e) => {
+    const answer = playRound(getComputerChoice(), "rock");
+    //passing answer because it's the result of playRound to update the score and reduce redundancy
+    scoreDisplay(answer);
+});
+    
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', (e) => {
+    const answer = playRound(getComputerChoice(), "paper");
+    scoreDisplay(answer);
+});
 
-console.log(game())
-// console.log(getComputerChoice())
-// console.log(playRound(getComputerChoice(),"Paper"))
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', (e) => {
+    const answer = playRound(getComputerChoice(), "scissors");
+    scoreDisplay(answer);
+});
+
+function getResult() {
+    if (playerScore >= 5) {
+        return "Congratulations, You Win!"
+    }
+    if (computerScore >= 5) {
+        return "Not this time, You Lose."
+    }
+}
+// now that buttons return lowercase strings, i just need to figure out how to get the
+// clicking of the buttons to be the choices
+            //OR!!
+//move hard coded function to each button choice since we won't be adding any buttons in the future
+
+
+
+                                // console.log(game())
+                                // console.log(getComputerChoice())
+                                // console.log(playRound(getComputerChoice(),"Paper"))
